069c7aa4d5d9e425d35877e62fa527c6f571d064
Samuel Schlesinger <sam@casperlabs.io>
2022-09-06 18:17:33 -0400

    made clear that restart doesn't work

--- src/systems/restart.rs
+++ src/systems/restart.rs
@@ -2,6 +2,7 @@ use bevy::prelude::*;
 
 use crate::state::GameState;
 
+// TODO Make this work
 pub fn restart(mut state: ResMut<State<GameState>>, keyboard_input: Res<Input<KeyCode>>) {
     if (*state.current() == GameState::Defeat || *state.current() == GameState::Victory)
         && keyboard_input.just_pressed(KeyCode::R)
